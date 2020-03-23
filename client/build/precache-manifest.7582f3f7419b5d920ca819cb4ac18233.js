700e154bcafabcdce84871c660f8b7f8cc3ab8fb
Phil <harrisonharry01@gmail.com>
2020-03-23 08:46:16 +0530

    Version 6.0

---
+++ client/build/precache-manifest.7582f3f7419b5d920ca819cb4ac18233.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac104b1f9719aa6a99ec36e0a3355c0f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0bd102cae3fa36aefe7c",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "32b8cb31ebefce56289b",
+    "url": "./static/css/main.11308d05.chunk.css"
+  },
+  {
+    "revision": "0bd102cae3fa36aefe7c",
+    "url": "./static/js/2.3adfc553.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.3adfc553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b8cb31ebefce56289b",
+    "url": "./static/js/main.f811f13d.chunk.js"
+  },
+  {
+    "revision": "ebc0353ede0d79edbeb9",
+    "url": "./static/js/runtime-main.544b41fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
